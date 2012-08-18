b01333c2f5566864bfd86de8f85055e09acf81ab
boxp <tiyotiyouda@gmail.com>
2012-08-18 23:52:50 +0900

    First commit

---
+++ tests/src/main/scala/Tests.scala
@@ -0,0 +1,20 @@
+package alice.scala.speca.tests
+
+import alice.scala.speca._
+import junit.framework.Assert._
+import _root_.android.test.AndroidTestCase
+import _root_.android.test.ActivityInstrumentationTestCase2
+
+class AndroidTests extends AndroidTestCase {
+  def testPackageIsCorrect() {
+    assertEquals("alice.scala.speca", getContext.getPackageName)
+  }
+}
+
+class ActivityTests extends ActivityInstrumentationTestCase2(classOf[MainActivity]) {
+   def testHelloWorldIsShown() {
+      val activity = getActivity
+      val textview = activity.findView(TR.textview)
+      assertEquals(textview.getText, "hello, world!")
+    }
+}
