e5456708f461b32f4c68211696fabee8ad270e33
BOXP <boxp@BOXP-laptop.(none)>
2012-10-04 00:44:22 +0900

    ビュークラスを追加

--- src/main/scala/MainActivity.scala
+++ src/main/scala/MainActivity.scala
@@ -1,13 +1,44 @@
 package alice.scala.speca
 
-import _root_.android.app.Activity
 import _root_.android.os.Bundle
+import _root_.android.app.Activity
+import _root_.android.widget.Toast
 
 class MainActivity extends Activity with TypedActivity {
+
   override def onCreate(bundle: Bundle) {
     super.onCreate(bundle)
     setContentView(R.layout.main)
+    Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show()
+  }
 
-    findView(TR.textview).setText("hello, world!")
+  override def onStart() {
+    super.onStart()
+    Toast.makeText(this, "onStart()", Toast.LENGTH_SHORT).show()
+  }
+
+  override def onRestart() {
+    super.onRestart()
+    Toast.makeText(this, "onRestart()", Toast.LENGTH_SHORT).show()
+  }
+
+  override def onResume() {
+    super.onRestart()
+    Toast.makeText(this, "onResume()", Toast.LENGTH_SHORT).show()
+  }
+  
+  override def onPause() {
+    super.onRestart()
+    Toast.makeText(this, "onPause()", Toast.LENGTH_SHORT).show()
+  }
+
+  override def onStop() {
+    super.onStop()
+    Toast.makeText(this, "onStop()", Toast.LENGTH_SHORT).show()
+  }
+
+  override def onDestroy() {
+    super.onDestroy()
+    Toast.makeText(this, "onDestroy()", Toast.LENGTH_SHORT).show()
   }
 }
---
+++ src/main/scala/MainView.scala
@@ -0,0 +1,15 @@
+package alice.scala.speca
+
+import _root_.android.content.Context
+import _root_.android.graphics.Canvas
+import _root_.android.view.View
+
+class MainView extends view {
+  SampleView(Context context){
+  super(context)
+  }
+
+  //描画処置
+  override def onDraw(Canvas c){
+  }
+}
